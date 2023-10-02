082f0853dd70e8a925ad5321e69a0b8ba2533831
N. Belva <121003978+p1xlb@users.noreply.github.com>
2023-10-03 06:14:04 +0800

    Delete POSTTEST3/script2.js

--- POSTTEST3/script2.js
+++
@@ -1,6 +0,0 @@
-const theme_button = document.getElementsByClassName('theme-button')
-tombol.addEventListener("click", function ubah(){
-    const container = document.getElementsByClassName('container');
-    
-}
-)
